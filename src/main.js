cc0442eb96faf941273535f90a4179d50e5e079b
Mark Hahn <mark@hahnca.com>
2025-04-05 12:54:48 -0700

    added globalMarks

--- src/main.js
+++ src/main.js
@@ -6,34 +6,38 @@ let context, globalMarks;
 
 function init(contextIn) { 
   context = contextIn;
+  context.workspaceState.update('globalMarks', {}); // DEBUG
   globalMarks = context.workspaceState.get('globalMarks', {});
   log('main initialized'); 
 }
 
 function getToken() {
-  const hashDigit = () => Math.floor(Math.random() * 36).toString(36);
+  const hashDigit = () => Math.floor(Math.random()*36).toString(36);
   let randHash;
   do {  randHash = ''; for(let i = 0; i < 4; i++) randHash += hashDigit()}
   while(randHash in globalMarks);
-  const token = `@${randHash}@`;
-  globalMarks[token] = {};
+  return `@${randHash}@`;
+}
+
+function addToGlobalMarks(data, token) {
+  token = token ?? getToken();
+  globalMarks[token] = data;
   context.workspaceState.update('globalMarks', globalMarks);
-  // log('generated token:', {token, globalMarks});
   return token;
 }
 
 function toggle() {
   log('toggle called');
-  
   const editor = vscode.window.activeTextEditor;
   if (!editor) { log('toggle, No active editor'); return; }
   const document   = editor.document;
   const languageId = document.languageId;
   const relPath    = vscode.workspace.asRelativePath(document.uri);
-  const lineNum    = editor.selection.active.line;
-  const token      = getToken();
-
-  log('getToken:', {languageId, relPath, lineNum, token, globalMarks});
+  const lineNumber = editor.selection.active.line;
+  const token      = addToGlobalMarks({languageId, relPath, lineNumber});
+  const line       = document.lineAt(lineNumber);
+  editor.edit(builder => {builder.insert(line.range.end, ` // ${token}`)});
+  log('toggle globalMarks:', globalMarks);
 }
 
 module.exports = { init, toggle };

5f7dfa70824a401915b0d3364f905f3d1f4b4068
Mark Hahn <mark@hahnca.com>
2025-04-26 11:04:55 -0700

    added invisible Base4 functions and tokens

--- src/utils.js
+++ src/utils.js
@@ -337,9 +337,57 @@ function debounce(fn, delay = 100) {
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
 
+function invBase4ToNumber(str) {
+  const digitMap = {
+    '\u200B': 0, // Zero Width Space
+    '\u200C': 1, // Zero Width Non-Joiner
+    '\u200D': 2, // Zero Width Joiner
+    '\u2060': 3  // Word Joiner
+  };
+  let num = 0;
+  for (const char of str) {
+    const digit = digitMap[char];
+    if (digit === undefined)
+      throw new Error('Invalid character in zero-width base-4 string');
+    num = num * 4 + digit;
+  }
+  return num;
+}
+
+function numberToInvBase4(num) {
+  const zeroWidthDigits = ['\u200B', '\u200C', '\u200D', '\u2060'];
+  if (num === 0) return zeroWidthDigits[0];
+  let result = '';
+  while (num > 0) {
+    const digit = num % 4;
+    result = zeroWidthDigits[digit] + result;
+    num = Math.floor(num / 4);
+  }
+  return result;
+}
+
+function getEmptyToken() {
+  return "\u200B.";
+}
+
+let uniqueId = 0;
+
+function getUniqueInvBase4Token(comLft, comRgt) {
+  return numberToInvBase4(++uniqueId)+'.';
+}
+
+function getInvBase4RegEx() {
+  return new regExp("[\\u200B\\u200C\\u200D\\u2060]+\\.");
+}
+
+function getInvBase4RegExG() {
+  return new regExp("[\\u200B\\u200C\\u200D\\u2060]+\\.", 'g');
+}
+
 module.exports = {
   init, getLog, fnv1aHash, loadStickyBookmarksJson,
   commentsByLang, keywords, fileExists, getLineFromTextAtOffset,
+  getEmptyToken, getUniqueInvBase4Token, getInvBase4RegEx, getInvBase4RegExG,
   deleteLine, insertLine, replaceLine, debounce, sleep,
   getPathsFromWorkspaceFolder, getPathsFromFileDoc,
   runOnAllFilesInFolder, getFocusedWorkspaceFolder, initProvider,

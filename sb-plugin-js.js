085b16282bf2710505f78ca030850798a8b52d96
Mark Hahn <mark@hahnca.com>
2025-04-28 23:15:25 -0700

    clnup plugin

--- sb-plugin-js.js
+++ sb-plugin-js.js
@@ -2,29 +2,30 @@
 // this is fast but speed isn't that important. Only runs on toggle click
 function findSafeLine(text, toggleLineNum) {
   const lines  = text.split(/\r?\n/);
+  if(toggleLineNum >= lines.length) return lines.length-1;
   let checking = false;
   let insideSingle, insideDouble;
   let insideTemplate = false, insideBlock = false;
-  for (let curLineNum = 0; curLineNum < lines.length; curLineNum++) {
-    let line = lines[curLineNum];
+  for (let lineNumber = 0; lineNumber < lines.length; lineNumber++) {
+    let line = lines[lineNumber];
     // combine lines that are split by a backslash
-    while(line.slice(-1) === '\\' && curLineNum < lines.length-1)
-      line = line.slice(0, -1) + lines[++curLineNum];
-    if(curLineNum >= toggleLineNum) checking = true;
+    while(line.slice(-1) === '\\' && lineNumber < lines.length-1)
+      line = line.slice(0, -1) + lines[++lineNumber];
+    if(lineNumber >= toggleLineNum) checking = true;
     insideSingle = false; 
     insideDouble = false;
     for (let charNum = 0; charNum < line.length; charNum++) {
       const char = line[charNum];
-      // check for escaped char
+      // skip escaped char
       if(char === '\\') { charNum++; continue; }
-      const next2Char = char + line[charNum+1];
+      const next2Char = char + (line[charNum+1] ?? '');
       if (insideBlock) {
         if (next2Char === '*/') { insideBlock = false; charNum++; }
         continue;
       }
-      if (insideSingle   && char === "'") { insideSingle   = false; continue; }
-      if (insideDouble   && char === '"') { insideDouble   = false; continue; }
-      if (insideTemplate && char === '`') { insideTemplate = false; continue; }
+      if (insideSingle  ) { if(char == "'"){ insideSingle   = false; } continue; }
+      if (insideDouble  ) { if(char == '"'){ insideDouble   = false; } continue; }
+      if (insideTemplate) { if(char == '`'){ insideTemplate = false; } continue; }
       // we are outside strings/comments
       // if single-line comment skip rest of line
       if (next2Char === '//') break; 
@@ -39,10 +40,11 @@ function findSafeLine(text, toggleLineNum) {
       if (char === '`') { insideTemplate = true; continue; }
     }
     // end of line
-    if(checking && !insideTemplate) return curLineNum;
+    if(checking && !insideTemplate) return lineNumber;
   }
-  // template string or block comment is open to end of file
-  // in either case it is safe to add at end of file
+  // end of file
+  // template string or block comment is still open
+  // in either case it is safe to add token
   return lines.length - 1;
 }
 
